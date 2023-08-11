7064993b39eb08612f7e10d900e9d0fa50036bb9
jxshnpreet <2002jashanpreet@gmail.com>
2023-08-11 12:14:23 +0530

    Interface Added and Reference created to that interface

--- src/main/java/com/company/samplerestapi/services/StudentService.java
+++ src/main/java/com/company/samplerestapi/services/StudentService.java
@@ -1,6 +1,8 @@
 package com.company.samplerestapi.services;
 
 import com.company.samplerestapi.model.Student;
+import com.company.samplerestapi.repository.StudentInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,35 +11,41 @@ import java.util.List;
 @Service
 public class StudentService
 {
+    @Autowired
+    private StudentInterface studentInterface;
+
 //    Dependency Injection - Auto-Wiring
 //    This is a data fetched from the Database
-    List<Student> students = new ArrayList<>(
-        Arrays.asList(
-                new Student("1","Student 1","CSE" ,"USET" ),
-                new Student("2","Student 2","CSE" ,"USET" ),
-                new Student("3","Student 3","BCA" ,"USET" ),
-                new Student("4","Student 4","MCA" ,"USET" ),
-                new Student("5","Student 5","MBBS" ,"Medical" )
-        )
-    );
+//    List<Student> students = new ArrayList<>(
+//        Arrays.asList(
+//                new Student("1","Student 1","CSE" ,"USET" ),
+//                new Student("2","Student 2","CSE" ,"USET" ),
+//                new Student("3","Student 3","BCA" ,"USET" ),
+//                new Student("4","Student 4","MCA" ,"USET" ),
+//                new Student("5","Student 5","MBBS" ,"Medical" )
+//        )
+//    );
     public List<Student> getAllStudents(){
+//        return students;
+        List<Student> students = new ArrayList<>();
+        studentInterface.findAll().forEach(student -> students.add(student));
         return students;
     }
 
-    public Student getStudentInfo(String r_no) {
-       return students.stream().filter(s -> s.getR_no().equals(r_no)).findFirst().get();
-    }
-
-    public void addStudent(Student student) {
-        students.add(student);
-    }
-    public void editStudent(String r_no, Student updatedStudent) {
-//        to check if the student is available in db or not
-        for (int i = 0; i < students.size(); i++) {
-            Student s = students.get(i);
-            if (s.getR_no().equals(r_no)){
-                students.set(i, updatedStudent);
-            }
-        }
-    }
+//    public Student getStudentInfo(String r_no) {
+//       return students.stream().filter(s -> s.getR_no().equals(r_no)).findFirst().get();
+//    }
+//
+//    public void addStudent(Student student) {
+//        students.add(student);
+//    }
+//    public void editStudent(String r_no, Student updatedStudent) {
+////        to check if the student is available in db or not
+//        for (int i = 0; i < students.size(); i++) {
+//            Student s = students.get(i);
+//            if (s.getR_no().equals(r_no)){
+//                students.set(i, updatedStudent);
+//            }
+//        }
+//    }
 }
